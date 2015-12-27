5fa9a1ca3988312e1870df1097b400aaaca1e046
Franz Schanovsky <franz.schanovsky@gmail.com>
2015-12-28 00:10:57 +0100

    NOT WORKING VERSION... It's just too late to continue. Added tasks to make code more readable and checks to detect timeouts and collisions.
    Signed-off-by: Franz Schanovsky <franz.schanovsky@gmail.com>

--- i2c_master.sv
+++ i2c_master.sv
@@ -1,10 +1,11 @@
 module i2c_master #(
-    START_DURATION=8'h1E,
-    DATA_SETUP_DELAY=8'h0,
-    DATA_HOLD_DELAY=8'h1E,
-    SCLK_HIGH_DURATION=8'h1E,
-    ACK_DURATION=8'h1E,
-    STOP_DURATION=8'h1E
+    START_DURATION     = 8'h1E,
+    DATA_SETUP_DELAY   = 8'h0,
+    DATA_HOLD_DELAY    = 8'h1E,
+    SCLK_HIGH_DURATION = 8'h1E,
+    ACK_DURATION       = 8'h1E,
+    STOP_DURATION      = 8'h1E,
+    TIMEOUT            = 8'hBF
 )(
     /* clock & reset */
     input  logic clock_i,
@@ -55,49 +56,94 @@ struct {
     logic [8-1:0] rw_buffer;
     logic         read_fifo;
     logic         write_fifo;
-    logic         sclk;
-    logic         sdat;
     int           data_idx;
     logic         error;
+    logic         sclk_timeout;
+    logic         collision_detected;
 } pres, next;
 
-logic [8-1:0] timer;
+delay_t       timer;
 logic         timer_start; 
-logic timer_expired;
+logic         timer_expired;
+logic         timeout_occured;
 logic [1:0]   write_fifo_delay;
 logic [1:0]   sclk_synchronizer;
 logic         sclk_sync;
 logic [1:0]   sdat_synchronizer;
 logic         sdat_sync;
+logic         last_sdat_output;
 
 multitimer #(
     .TIMER_WIDTH(8),
-    .INTERRUPTS(1)
+    .INTERRUPTS(2)
     ) mtimer (
-    .reset_n_i     ( reset_n_i        ),
-    .clock_i       ( clock_i          ),
-    .timer_set_i   ( '{timer}    ),
-    .timer_start_i ( timer_start ),
-    .expired_o     ( '{timer_expired} )
+    .reset_n_i     ( reset_n_i                         ),
+    .clock_i       ( clock_i                           ),
+    .timer_set_i   ( '{timer, delay_t'(TIMEOUT)}       ),
+    .timer_start_i ( timer_start                       ),
+    .expired_o     ( '{timer_expired, timeout_occured} )
     );
 
-always_ff @(posedge clock_i, negedge reset_n_i)
-begin
-    if (!reset_n_i) begin
-        pres.phys_state  <= I2C_IDLE;
-        pres.data_idx    <= 0;
-        pres.len_read    <= '0;
-        pres.sclk        <= 1'b1;
-        pres.sdat        <= 1'b1;
-        pres.read_fifo   <= 1'b0;
-        pres.write_fifo  <= 1'b0;
-        pres.error       <= 1'b0;
-        pres.rw_buffer   <= 8'b0;
-    end else begin
-        pres <= next;
-    end
-end
+/*
+ * Returns the timer delay for a given state. Returns 0 for unknown states.
+ */
+function delay_t delay_for_state(phys_state_t state);
+    delay_for_state = (state == I2C_START              ) ? delay_t'(START_DURATION    )
+                    : (state == I2C_WR_BIT_SET_SCLK    ) ? delay_t'(DATA_SETUP_DELAY  )
+                    : (state == I2C_WR_BIT_SET_SDAT    ) ? delay_t'(DATA_HOLD_DELAY   )
+                    : (state == I2C_WR_BIT_HOLD_SDAT   ) ? delay_t'(SCLK_HIGH_DURATION)
+                    : (state == I2C_ACK_SET_SCLK       ) ? delay_t'(DATA_SETUP_DELAY  )
+                    : (state == I2C_ACK_SET_SDAT       ) ? delay_t'(DATA_HOLD_DELAY   )
+                    : (state == I2C_ACK_HOLD           ) ? delay_t'(SCLK_HIGH_DURATION)
+                    : (state == I2C_RD_BIT_SET         ) ? delay_t'(DATA_SETUP_DELAY + DATA_HOLD_DELAY)
+                    : (state == I2C_RD_BIT_HOLD        ) ? delay_t'(SCLK_HIGH_DURATION)
+                    : (state == I2C_SLAVE_ACK_SET_SCLK ) ? delay_t'(DATA_SETUP_DELAY  )
+                    : (state == I2C_SLAVE_ACK_SET_SDAT ) ? delay_t'(DATA_HOLD_DELAY   )
+                    : (state == I2C_SLAVE_ACK_HOLD     ) ? delay_t'(SCLK_HIGH_DURATION)
+                    : (state == I2C_STOP_SET_SCLK      ) ? delay_t'(STOP_DURATION     )
+                    : (state == I2C_STOP_SET_SDAT      ) ? delay_t'(DATA_HOLD_DELAY   )
+                    : (state == I2C_STOP_DELAY         ) ? delay_t'(SCLK_HIGH_DURATION)
+                    : (state == I2C_STOP               ) ? delay_t'(STOP_DURATION     )
+                    : delay_t'(0);
+endfunction
 
+task setup_state(input phys_state_t next_state);
+    timer           = delay_for_state(next_state);
+    timer_start     = 1'b1;
+    next.phys_state = next_state;
+endtask
+
+task raise_error();
+    next.error = 1'b1;
+    setup_state(I2C_STOP_SET_SCLK);
+endtask
+
+task check_timeout();
+    if (timeout_occured) raise_error(); 
+endtask
+
+task check_sclk();
+    if (i2c_sclk_o != sclk_sync) begin
+        next.collision_detected = 1'b1;
+        raise_error();
+    end
+endtask
+
+task check_sdat();
+    if (i2c_sdat_o != sdat_sync) begin
+        next.collision_detected = 1'b1;
+        raise_error();
+    end
+endtask
+
+task_check_lines();
+    check_sclk();
+    check_sdat();
+endtask
+
+/*
+ * State machine combinatorial block.
+ */
 always_comb
 begin
     next.phys_state = pres.phys_state;
@@ -107,7 +153,7 @@ begin
         busy_o = 1'b1;
 
     timer_start = 1'b0;
-    timer       = '0;
+    timer       = delay_t'(0);
     error_o          = 1'b0;
     next.read_fifo   = 1'b0;
     next.write_fifo  = 1'b0;
@@ -120,220 +166,172 @@ begin
 
     unique case (pres.phys_state)
         I2C_IDLE: begin
-            next.sclk  = 1'b1;
-            next.sdat  = 1'b1;
-            next.error = 1'b0;
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = 1'b1;
+            /* Set all internal states to default */
+            next.error      = 1'b0;
+            next.len_read   = 8'b0;
+            next.rw_buffer  = 8'b0;
+            next.read_fifo  = 1'b0;
+            next.write_fifo = 1'b0;
+            next.data_idx   = 0;
             if (cmd_strobe_i) begin
                 next.rw_buffer   = ctrl_wr_i;
-                next.phys_state  = I2C_START;
                 next.len_read    = len_rd_i;
-                timer       = delay_t'(START_DURATION);
-                timer_start = 1'b1;
                 busy_o = 1'b1;
+                setup_state(I2C_START);
             end
         end
         I2C_START: begin
-            next.sclk = 1'b1;
-            next.sdat = 1'b0;
-
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = 1'b0;
             if (timer_expired) begin
-                next.phys_state = I2C_WR_BIT_SET_SCLK;
                 next.data_idx   = 7;
-                timer_start     = 1'b1;
-                timer           = delay_t'(DATA_SETUP_DELAY);
+                setup_state(I2C_WR_BIT_SET_SCLK);
             end
+            check_sclk()
         end
         I2C_WR_BIT_SET_SCLK: begin
-            next.sclk = 1'b0;
-            next.sdat = pres.sdat;
-            if (timer_expired) begin
-                next.phys_state  = I2C_WR_BIT_SET_SDAT;
-                timer       = delay_t'(DATA_HOLD_DELAY);
-                timer_start = 1'b1;
-            end
+            i2c_sclk_o = 1'b0;
+            i2c_sdat_o = last_sdat_output;
+            if (timer_expired) setup_state(I2C_WR_BIT_SET_SDAT);
+            check_sdat()
         end
         I2C_WR_BIT_SET_SDAT: begin
-            next.sclk = timer_expired;
-            next.sdat = pres.rw_buffer[pres.data_idx];
-            if (sclk_sync) begin // We need to listen to the bus instead of the internal
-                                 // signal to make slave-induced delays possible.
-                next.phys_state = I2C_WR_BIT_HOLD_SDAT;
-                timer_start     = 1'b1;
-                timer           = delay_t'(SCLK_HIGH_DURATION);
-            end
+            i2c_sclk_o = timer_expired;
+            i2c_sdat_o = pres.rw_buffer[pres.data_idx];
+            if (sclk_sync) setup_state(I2C_WR_BIT_HOLD_SDAT);
+            check_timeout();
         end
         I2C_WR_BIT_HOLD_SDAT: begin
-            next.sclk = 1'b1;
-            next.sdat = pres.sdat;
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = last_sdat_output;
             if (timer_expired) begin
                 if (pres.data_idx > 0) begin
                     next.data_idx   = pres.data_idx - 1;
-                    timer           = delay_t'(DATA_SETUP_DELAY);
-                    timer_start     = 1'b1;
-                    next.phys_state = I2C_WR_BIT_SET_SCLK;
+                    setup_state(I2C_WR_BIT_SET_SCLK);
                 end else begin
-                    next.phys_state = I2C_ACK_SET_SCLK;
-                    timer           = delay_t'(DATA_SETUP_DELAY);
-                    timer_start     = 1'b1;
+                    setup_state(I2C_ACK_SET_SCLK);
                 end
             end
+            check_lines();
         end
         I2C_ACK_SET_SCLK: begin
-            next.sclk = 1'b0;
-            next.sdat = pres.sdat;
-            if (timer_expired) begin
-                next.phys_state = I2C_ACK_SET_SDAT;
-                timer           = delay_t'(DATA_HOLD_DELAY);
-                timer_start     = 1'b1;
-            end
+            i2c_sclk_o = 1'b0;
+            i2c_sdat_o = last_sdat_output;
+            if (timer_expired) setup_state(I2C_ACK_SET_SDAT);
+            check_sdat();
         end
         I2C_ACK_SET_SDAT: begin
-            next.sclk = timer_expired;
-            next.sdat = 1'b1;
-            if (sclk_sync) begin
-                next.phys_state = I2C_ACK_HOLD;
-                timer           = delay_t'(SCLK_HIGH_DURATION);
-                timer_start     = 1'b1;
-            end
+            i2c_sclk_o = timer_expired;
+            i2c_sdat_o = 1'b1;
+            if (sclk_sync) setup_state(I2C_ACK_HOLD);
+            check_timeout();
         end
         I2C_ACK_HOLD: begin
-            next.sclk = 1'b1;
-            next.sdat = 1'b1;
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = 1'b1;
             if (timer_expired) begin
                 if (sdat_sync) begin // ACK unsuccessful. -> error state.
-                    next.phys_state = I2C_STOP_SET_SCLK;
-                    next.error = 1'b1;
-                    timer = delay_t'(DATA_SETUP_DELAY);
-                    timer_start = 1'b1;
+                    raise_error();
                 end else begin
                     if (data_available_i) begin 
                         if (!pres.read_fifo) begin
                             next.read_fifo  = 1'b1;
                         end else begin
                             next.rw_buffer  = data_i;
-                            next.phys_state = I2C_WR_BIT_SET_SCLK;
                             next.data_idx   = 7;
-                            timer_start     = 1'b1;
-                            timer           = delay_t'(DATA_SETUP_DELAY);
+                            setup_state(I2C_WR_BIT_SET_SCLK);
                         end
                     end else begin
                         // No data left in fifo ... do reads now.
                         if (pres.len_read > 8'h0) begin
-                            next.phys_state = I2C_RD_BIT_SET;
-                            timer           = delay_t'(DATA_SETUP_DELAY + DATA_HOLD_DELAY);
-                            timer_start     = 1'b1;
                             next.data_idx   = 7;
                             next.len_read   = pres.len_read - 8'h1;
+                            setup_state(I2C_RD_BIT_SET);
                         end else begin
                             // No reads to be done ... stop
-                            next.phys_state = I2C_STOP_SET_SCLK;
-                            timer           = delay_t'(DATA_SETUP_DELAY);
-                            timer_start     = 1'b1;
+                            setup_state(I2C_STOP_SET_SCLK);
                         end
                     end
                 end
             end
+            check_sclk();
         end
         I2C_RD_BIT_SET: begin
-            next.sclk = timer_expired;
-            next.sdat = 1'b1;
-            if (sclk_sync) begin
-                next.phys_state = I2C_RD_BIT_HOLD;
-                timer           = delay_t'(SCLK_HIGH_DURATION);
-                timer_start     = 1'b1;
-            end
+            i2c_sclk_o = timer_expired;
+            i2c_sdat_o = 1'b1;
+            if (sclk_sync) setup_state(I2C_RD_BIT_HOLD);
+            check_timeout();
         end
         I2C_RD_BIT_HOLD: begin
-            next.sclk = 1'b1;
-            next.sdat = 1'b1; 
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = 1'b1; 
             if (timer_expired) begin
                 next.rw_buffer[pres.data_idx] = sdat_sync;
                 if (pres.data_idx == 0) begin
                     next.write_fifo = 1'b1;
-                    next.phys_state = I2C_SLAVE_ACK_SET_SCLK;
-                    timer           = delay_t'(DATA_SETUP_DELAY);
-                    timer_start     = 1'b1;
+                    setup_state(I2C_SLAVE_ACK_SET_SCLK);
                 end else begin
                     next.data_idx = pres.data_idx - 1; 
-                    next.phys_state = I2C_RD_BIT_SET;
-                    timer = delay_t'(DATA_SETUP_DELAY + DATA_HOLD_DELAY);
-                    timer_start = 1'b1;
+                    setup_state(I2C_RD_BIT_SET);
                 end
             end
+            check_sclk();
         end
         I2C_SLAVE_ACK_SET_SCLK: begin
-            next.sclk = 1'b0;
-            next.sdat = 1'b1;
-            if (timer_expired) begin
-                next.phys_state = I2C_SLAVE_ACK_SET_SDAT;
-                timer           = delay_t'(DATA_HOLD_DELAY);
-                timer_start     = 1'b1;
-            end
+            i2c_sclk_o = 1'b0;
+            i2c_sdat_o = 1'b1;
+            if (timer_expired) setup_state(I2C_SLAVE_ACK_SET_SDAT);
+            // Add checks
         end
         I2C_SLAVE_ACK_SET_SDAT: begin
-            next.sclk = timer_expired;
-            next.sdat = 1'b0;
-            if (sclk_sync) begin
-                next.phys_state = I2C_SLAVE_ACK_HOLD;
-                timer           = delay_t'(SCLK_HIGH_DURATION);
-                timer_start     = 1'b1;
-            end
+            i2c_sclk_o = timer_expired;
+            i2c_sdat_o = 1'b0;
+            if (sclk_sync) setup_state(I2C_SLAVE_ACK_HOLD);
+            check_timeout();
+            // Add checks
         end
         I2C_SLAVE_ACK_HOLD: begin
-            next.sclk = 1'b1;
-            next.sdat = 1'b0;
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = 1'b0;
             if (timer_expired) begin
                 data_valid_o = 1'b1;
                 if (pres.len_read > 8'h0) begin
                     next.len_read   = pres.len_read - 8'h1;
-                    next.phys_state = I2C_RD_BIT_SET;
                     next.data_idx   = 7;
-                    timer           = delay_t'(DATA_SETUP_DELAY + DATA_HOLD_DELAY);
-                    timer_start     = 1'b1;
+                    setup_state(I2C_RD_BIT_SET);
                 end else begin
-                    next.phys_state = I2C_STOP_SET_SCLK;
-                    timer           = STOP_DURATION;
-                    timer_start     = 1'b1;
+                    setup_state(I2C_STOP_SET_SCLK);
                 end
             end
+            // Add checks
         end
         I2C_STOP_SET_SCLK: begin
             // Intended to set up an unplanned stop signal.
-            next.sclk = 1'b0;
-            next.sdat = pres.sdat;
-            if (timer_expired) begin
-                next.phys_state = I2C_STOP_SET_SDAT;
-                timer = delay_t'(DATA_HOLD_DELAY);
-                timer_start = 1'b1;
-            end
+            i2c_sclk_o = 1'b0;
+            i2c_sdat_o = last_sdat_output;
+            if (timer_expired) setup_state(I2C_STOP_SET_SDAT);
         end
         I2C_STOP_SET_SDAT: begin
-            next.sclk = timer_expired;
-            next.sdat = 1'b0;
-            if (sclk_sync) begin
-                next.phys_state = I2C_STOP_DELAY;
-                timer = delay_t'(SCLK_HIGH_DURATION);
-                timer_start = 1'b1;
-            end
+            i2c_sclk_o = timer_expired;
+            i2c_sdat_o = 1'b0;
+            if (sclk_sync) setup_state(I2C_STOP_DELAY);
         end
         I2C_STOP_DELAY: begin
             // Entering this state we assume that SDAT is pulled low either by the master
             // (myself) or the slave, and that SCLK is high.
             // 
             // Then go to stop.
-            next.sclk = 1'b1;
-            next.sdat = 1'b0;
-            if (timer_expired) begin
-                next.phys_state = I2C_STOP;
-                timer = delay_t'(STOP_DURATION);
-                timer_start = 1'b1;
-            end
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = 1'b0;
+            if (timer_expired) setup_state(I2C_STOP);
         end
         I2C_STOP: begin
             // On the output side, this state is equivalent to idle. It exists just to make
             // sure that we wait some time before the next transaction begins.
-            next.sclk = 1'b1;
-            next.sdat = 1'b1;
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = 1'b1;
             if (timer_expired) begin
                 if (pres.error)
                     next.phys_state = I2C_ERROR;
@@ -342,14 +340,32 @@ begin
             end
         end
         I2C_ERROR: begin
-            next.sclk = 1'b1;
-            next.sdat = 1'b1;
+            i2c_sclk_o = 1'b1;
+            i2c_sdat_o = 1'b1;
             error_o   = 1'b1;
             if (ack_error_i) begin
                 next.phys_state = I2C_IDLE;
             end
         end
     endcase;
+end
+
+/*
+ * State machine register part.
+ */
+always_ff @(posedge clock_i, negedge reset_n_i)
+begin
+    if (!reset_n_i) begin
+        pres.phys_state  <= I2C_IDLE;
+        pres.data_idx    <= 0;
+        pres.len_read    <= '0;
+        pres.read_fifo   <= 1'b0;
+        pres.write_fifo  <= 1'b0;
+        pres.error       <= 1'b0;
+        pres.rw_buffer   <= 8'b0;
+    end else begin
+        pres <= next;
+    end
 end
 
 always_ff @(posedge clock_i or negedge reset_n_i)
@@ -360,6 +376,14 @@ begin
         write_fifo_delay[0] <= pres.write_fifo;
         write_fifo_delay[1] <= write_fifo_delay[0];
     end
+end
+
+always_ff @(posedge clock_i or negedge reset_n_i)
+begin
+    if (!reset_n_i)
+        last_sdat_output <= 1'b0;
+    else
+        last_sdat_output <= i2c_sdat_o;
 end
 
 // SCLK and SDAT synchronization
@@ -378,8 +402,6 @@ begin
     end
 end
 
-assign i2c_sclk_o  = pres.sclk;
-assign i2c_sdat_o  = pres.sdat;
 assign data_read_o = pres.read_fifo;
 assign data_o      = pres.rw_buffer;
 
